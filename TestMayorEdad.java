fdffc141027c4523ff87a7bc1a4497ee2ba1d943
EscoriaRebelde <124387382+EscoriaRebelde@users.noreply.github.com>
2023-04-14 22:25:57 +0100

    Ejercicio Test de JUnit 5 hecho el 14 de abril de 2023 en clase.

---
+++ TestMayorEdad.java
@@ -0,0 +1,46 @@
+package MayorDeEdad;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.Test;
+
+	public class TestMayorEdad {
+
+		
+		@Test(expected=AssertionError.class)
+		public void test1() {
+			Persona p = new Persona(-1);
+			p.isMayorDeEdad();
+		}
+		
+		@Test(expected=AssertionError.class)
+		public void test2() {
+			Persona p = new Persona(-100);
+			p.isMayorDeEdad();
+		}
+		
+		@Test public void test3() {
+			Persona p = new Persona(0);
+			assertFalse(p.isMayorDeEdad());
+		}
+		
+		@Test public void test4() {
+			Persona p = new Persona(17);
+			assertFalse(p.isMayorDeEdad());
+		}
+		
+		@Test public void test5() {
+			Persona p = new Persona(18);
+			assertTrue(p.isMayorDeEdad());
+		}
+		
+		@Test public void test6() {
+			Persona p = new Persona(100);
+			assertTrue(p.isMayorDeEdad());
+		}
+		
+		
+	}
+
+
